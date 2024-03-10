99eb7884103fefe1b7f0cf3fd78653303f333d2c
govor <govorinsasha0@gmail.com>
2024-03-10 18:45:23 +0100

    Improved

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,26 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int[] moneySpendPerMonth = new int[]{1,2,3,4,5};
-        for (int j : moneySpendPerMonth) {
-            System.out.println("Сумма трат за месяц составила " + j);
+        int[] moneySpendPerMonth = new int[] {1,2,3,4,5};
+        for (int moneyLoss : moneySpendPerMonth) {
+            System.out.println("Сумма трат за месяц составила " + moneyLoss);
         }
-        int[] moneySpendPerWeek = new int[]{2,3,4,5,6};
+        int[] moneySpendPerWeek = new int[] {2,3,4,5,6};
         int biggestLoss = moneySpendPerWeek[1];
         int smallestLoss = moneySpendPerWeek[1];
-        for (int j : moneySpendPerWeek) {
-            if (biggestLoss < j) {
-                biggestLoss = j;
+        for (int moneyLoss : moneySpendPerWeek) {
+            if (biggestLoss < moneyLoss) {
+                biggestLoss = moneyLoss;
             }
-            if (smallestLoss > j){
-                smallestLoss = j;
+            if (smallestLoss > moneyLoss){
+                smallestLoss = moneyLoss;
             }
         }
-        System.out.println("Минимальная сумма трат за неделю составила " + biggestLoss + " рублей.\nМаксимальная сумма трат за неделю составила " + smallestLoss + " рублей");
+        System.out.println("Минимальная сумма трат за неделю составила " + smallestLoss + " рублей.\nМаксимальная сумма трат за неделю составила " + biggestLoss + " рублей");
 
         int[] weeklySpend = {1, 2, 3, 4, 5};
         int totalSpend = 0;
-        for (int i : weeklySpend) {
-            totalSpend += i;
+        for (int moneyLoss : weeklySpend) {
+            totalSpend += moneyLoss;
         }
         double averageExpenses = (double) totalSpend / weeklySpend.length;
         System.out.println("Средняя сумма трат за месяц составила " + averageExpenses + " рублей");
